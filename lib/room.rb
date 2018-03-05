a7717bc2da3849b0dfb44344c08c9cd721b6f318
Karinna Iniguez <karinnainiguez@gmail.com>
2018-03-05 14:21:28 -0800

    Add tests and functionality for Room#price

--- lib/room.rb
+++ lib/room.rb
@@ -5,15 +5,14 @@ module  Hotel
   class Room
 
     attr_reader :number
+    attr_accessor :price
 
-    def initialize(number)
-      if number.class != Integer || number < 1
+    def initialize(data)
+      if data[:number] == nil || data[:number].class != Integer || data[:number] < 1
         raise ArgumentError.new("Rooms must have a positive integer for room number. Received #{number}")
       end
-      @number = number
-
-
-
+      @number = data[:number]
+      @price = data[:price].to_f ||= 200.00
     end
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -5,32 +5,45 @@ describe "Room Class" do
   describe "#initialize" do
 
     it "can be created" do
-      result = Hotel::Room.new(4)
+      data = {number: 4, price: 400}
+      result = Hotel::Room.new(data)
       result.must_be_kind_of Hotel::Room
     end
 
     it "takes a room number" do
-      room = Hotel::Room.new(4)
+      data = {number: 4, price: 400}
+      room = Hotel::Room.new(data)
       room.must_respond_to :number
     end
 
     it "has a room number that is an integer" do
-      room = Hotel::Room.new(4)
+      data = {number: 4}
+      room = Hotel::Room.new(data)
       room.number.must_be_kind_of Integer
     end
 
     it "raises an argument if incorrect room number provided" do
+      data = {number: "whatever", price: 400}
       proc{
-        room = Hotel::Room.new("whatever")
+        room = Hotel::Room.new(data)
       }.must_raise ArgumentError
+      data = {number: 4.56, price: 400}
       proc{
-        room = Hotel::Room.new(4.5)
+        room = Hotel::Room.new(data)
       }.must_raise ArgumentError
     end
 
+    it "has a price that is a float" do
+      room = Hotel::Room.new({number: 5})
+      room.must_respond_to :price
+      room.price.must_be_kind_of Float
+    end
 
-
-
+    it "accepts an optional price" do
+      price = 456.82
+      room = Hotel::Room.new({number: 5, price: price})
+      room.price.must_equal price
+    end
 
 
   end # initialize
