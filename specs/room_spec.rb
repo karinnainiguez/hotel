ef50e2c1397c13b461bfb8e55047208b535930eb
Karinna Iniguez <karinnainiguez@gmail.com>
2018-03-05 12:26:06 -0800

    Add directories, Rakefile, Coverage directory, and room production and spec files.

---
+++ specs/room_spec.rb
@@ -0,0 +1,8 @@
+require_relative 'spec_helper'
+
+describe "Testing Tests" do
+  it "this should fail" do
+    result = 6
+    result.must_be_nil
+  end
+end
---
+++ specs/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative '../lib/room'
